2f46c6ab6a262b42102ee9ddff0b6823250261c9
Stella <wanlly1993@gmail.com>
2022-05-31 16:22:06 +0800

    3rd Version

--- GitSample/src/TestAPP/TestAPP.java
+++ GitSample/src/TestAPP/TestAPP.java
@@ -4,7 +4,10 @@ public class TestAPP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("2nd Version");
+		//我是組員，我要修改檔案
+		System.out.println("3rd Version");
 	}
 
 }
+
